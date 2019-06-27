50a0ffc076a66565138f9f4fd7b3801f1beea27e
SteppenHorde <steppenhorde@yandex.ru>
2019-06-27 17:31:50 +0300

    initial core/urls

---
+++ core/urls.py
@@ -0,0 +1,15 @@
+from django.urls import include, path
+from django.conf import settings
+from django.conf.urls.static import static
+
+from . import views
+
+
+
+urlpatterns = [
+    path('', views.Main.as_view(), name='main'),
+    path('books/', views.Books.as_view(), name='books'),
+    path('books/<int:blog_id>', views.ShowBook.as_view(), name='show_book'),
+    path('authors', views.Authors.as_view(), name='author'),
+    path('authors/<int:blog_id>', views.ShowAuthor.as_view(), name='show_author'),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
